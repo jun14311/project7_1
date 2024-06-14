799f3f0572f4ad7b8554af6d24d1edc355079412
hjun <dkdldb14311@naver.com>
2024-06-14 11:14:20 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+//import { ArrowRight, ArchiveFill, Router } from "react-bootstrap-icons"
+import { Routes, Route } from 'react-router-dom'
+import ProductAll from './page/ProductAll';
+import LoginPage from './page/LoginPage';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './page/Navbar';
+import { useEffect, useState } from 'react';
+import PrivateRoute from './page/PrivateRoute';
 
 function App() {
+  //false는 로그인 안된 상태, true는 로그인한 상태
+  const [authenticate, setauthenticate] = useState(false);
+  
+  useEffect(() => {
+    console.log("로그인 상태 확인", authenticate);
+  }, [authenticate]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<ProductAll />} />
+      <Route path='/login' element={<LoginPage setauthenticate = {setauthenticate} />} />
+      <Route path='/productdetail' element={<ProductDetail />} />
+      <Route path='/product/:id' element={<PrivateRoute authenticate = {authenticate} />} />
+    </Routes>
+   </>
   );
 }
 
 export default App;
+
